8bdd0ec607f1a283ab6e963ac613e22b04264180
Paul Philips <paulphilips@email.com>
2026-02-03 11:45:32 +0530

    Add project files.

---
+++ MyProject11/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyProject11.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
